ebca6de2b1c585758007a783e7a549cf5710c42c
Tran Duy Nghia <dev10.katec@gmail.com>
2026-01-25 19:49:31 +0700

    fix (bug): mobile carousel

--- src/components/FeaturedMovieSlider.tsx
+++ src/components/FeaturedMovieSlider.tsx
@@ -17,6 +17,8 @@ export default function FeaturedMovieSlider({ movies }: FeaturedMovieSliderProps
     slidesToShow: 6,
     slidesToScroll: 1,
     arrows: true,
+    rows: 1,
+    slidesPerRow: 1,
     responsive: [
       { breakpoint: 1280, settings: { slidesToShow: 5 } },
       { breakpoint: 1024, settings: { slidesToShow: 4 } },
@@ -25,30 +27,35 @@ export default function FeaturedMovieSlider({ movies }: FeaturedMovieSliderProps
         breakpoint: 640,
         settings: {
           slidesToShow: 2,
+          slidesToScroll: 2,
           arrows: false,
+          rows: 1,
+          slidesPerRow: 1,
         },
       },
     ],
   };
 
   return (
-    <Slider {...sliderSettings}>
-      {movies.map((movie) => (
-        <div key={movie._id} className="px-2">
-          <div className="scale-105 sm:scale-100">
-            <Link href={`/phim/${movie.slug}`}>
-              <MovieCard
-                movie={{
-                  ...movie,
-                  thumb_url: movie.thumb_url?.startsWith('http')
-                    ? movie.thumb_url
-                    : `https://phimimg.com/${movie.thumb_url}`,
-                }}
-              />
-            </Link>
+    <div className="w-full mobile-slider-fix">
+      <Slider {...sliderSettings}>
+        {movies.map((movie) => (
+          <div key={movie._id} className="px-1 sm:px-2">
+            <div className="w-full">
+              <Link href={`/phim/${movie.slug}`}>
+                <MovieCard
+                  movie={{
+                    ...movie,
+                    thumb_url: movie.thumb_url?.startsWith('http')
+                      ? movie.thumb_url
+                      : `https://phimimg.com/${movie.thumb_url}`,
+                  }}
+                />
+              </Link>
+            </div>
           </div>
-        </div>
-      ))}
-    </Slider>
+        ))}
+      </Slider>
+    </div>
   );
 }
